bf318e2421fcc2e197d4aa6daa64499ce912ce41
Son Do <dominhson580@gmail.com>
2023-10-22 11:58:51 +0700

    Define possible relationship statuses

---
+++ server/constants/index.js
@@ -0,0 +1,17 @@
+const RELATIONSHIP_STATUSES = [
+  "Single",
+  "In a relationship",
+  "Engaged",
+  "Married",
+  "In a civil union",
+  "In a domestic partnership",
+  "In an open relationship",
+  "It's complicated",
+  "Separated",
+  "Divorced",
+  "Widowed",
+];
+
+module.exports = {
+  RELATIONSHIP_STATUSES,
+};
