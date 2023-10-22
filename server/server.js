c88c0b6cbd2cb051ca54609a8c45d0682852cd41
Son Do <dominhson580@gmail.com>
2023-10-22 10:07:50 +0700

    Config dotenv to use environment variables

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors()); // should define allowedOrigins in production
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Facebook");
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
