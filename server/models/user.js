93cdc19b933b8dd88b698b29662c882eb54e6192
Son Do <dominhson580@gmail.com>
2023-10-22 21:14:37 +0700

    Fix syntax error and change field name

--- server/models/user.js
+++ server/models/user.js
@@ -12,44 +12,44 @@ const userSchema = Schema(
   {
     firstName: {
       type: String,
-      require: [true, "First Name is required"],
+      required: [true, "First Name is required"],
       trim: true,
     },
     lastName: {
       type: String,
-      require: [true, "Last Name is required"],
+      required: [true, "Last Name is required"],
       trim: true,
     },
-    userName: {
+    username: {
       type: String,
-      require: [true, "Username is required"],
+      required: [true, "Username is required"],
       trim: true,
       unique: true,
     },
     email: {
       type: String,
-      require: [true, "Email is required"],
+      required: [true, "Email is required"],
       trim: true,
     },
     password: {
       type: String,
-      require: [true, "Password is required"],
+      required: [true, "Password is required"],
     },
     birthDay: {
       type: Number,
-      require: [true, "Birth day is required"],
+      required: [true, "Birth day is required"],
     },
     birthMonth: {
       type: Number,
-      require: [true, "Birth month is required"],
+      required: [true, "Birth month is required"],
     },
     birthYear: {
       type: Number,
-      require: [true, "Birth year is required"],
+      required: [true, "Birth year is required"],
     },
     gender: {
       type: String,
-      require: [true, "Gender is required"],
+      required: [true, "Gender is required"],
       trim: true,
     },
     verified: {
