23ca0644953b0eab21fd3ecca78cfabd52fac34f
Son Do <dominhson580@gmail.com>
2023-10-22 15:33:50 +0700

    Use userRouter for '/' path

--- server/server.js
+++ server/server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// connect to database
 const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
-
 app.use(cors()); // should define allowedOrigins in production
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+const userRouter = require("./routes/user");
+app.use("/", userRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
