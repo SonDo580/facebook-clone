8873f64f0ff8f6abaa455879a562121eee1bb51c
Son Do <dominhson580@gmail.com>
2023-10-22 09:47:37 +0700

    Config CORS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors()); // should define allowedOrigins in production
 
 app.get("/", (req, res) => {
   res.send("Welcome to Facebook");
