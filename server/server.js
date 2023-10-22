deddbd468cbe532a739c06b45587d718b779dcea
Son Do <dominhson580@gmail.com>
2023-10-22 10:50:16 +0700

    Add middleware to parse request body

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
-
 connectDB();
 
 const app = express();
+
 app.use(cors()); // should define allowedOrigins in production
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
