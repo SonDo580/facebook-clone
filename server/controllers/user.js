8dca0c89058d4dab98dbaff3a4a2d170cf462703
Son Do <dominhson580@gmail.com>
2023-10-23 11:21:39 +0700

    Validate and sanitize input

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,53 +1,91 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 const User = require("../models/user");
 const { getUserInfo, getHashedPassword } = require("../utils/auth");
 
-const register = asyncHandler(async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    username,
-    password,
-    birthDay,
-    birthMonth,
-    birthYear,
-    gender,
-  } = req.body;
+const registerValidations = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("First Name is required!")
+    .isLength({ max: 30 })
+    .withMessage("First Name must contains at most 30 characters!")
+    .escape(),
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .withMessage("Last Name is required!")
+    .isLength({ max: 30 })
+    .withMessage("Last Name must contains at most 30 characters!")
+    .escape(),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required!")
+    .isEmail()
+    .withMessage("Email is invalid!")
+    .normalizeEmail(),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required!")
+    .isLength({ min: 6, max: 40 })
+    .withMessage("Password must be between 6 and 40 characters!")
+    .matches("")
+    .withMessage("Password must contains alphanumeric and special characters!"),
+];
 
-  // Validate input
+const register = [
+  registerValidations,
+  asyncHandler(async (req, res) => {
+    // Check validation errors
+    const errors = validationResult(req).array();
+    if (errors.length > 0) {
+      res.status(400);
+      throw new Error(errors[0].msg);
+    }
 
-  // Check if username is available
-  const existedUser = await User.findOne({ username });
-  if (existedUser) {
-    res.status(400);
-    throw new Error("Username already exists!");
-  }
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      birthDay,
+      birthMonth,
+      birthYear,
+      gender,
+    } = req.body;
 
-  // Hash password
-  const hashedPassword = await getHashedPassword(password);
+    // Check if email existed
+    const existedUser = await User.findOne({ email });
+    if (existedUser) {
+      res.status(400);
+      throw new Error("Email already used!");
+    }
 
-  // Create and save user
-  const user = await User.create({
-    firstName,
-    lastName,
-    email,
-    username,
-    birthDay,
-    birthMonth,
-    birthYear,
-    gender,
-    password: hashedPassword,
-  });
+    // Hash password
+    const hashedPassword = await getHashedPassword(password);
 
-  if (!user) {
-    res.status(400);
-    throw new Error("Invalid user data!");
-  }
+    // Create and save user
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      birthDay,
+      birthMonth,
+      birthYear,
+      gender,
+    });
 
-  res.status(201).json(getUserInfo(user));
-});
+    if (!user) {
+      res.status(400);
+      throw new Error("Invalid user data!");
+    }
+
+    res.status(201).json(getUserInfo(user));
+  }),
+];
 
 module.exports = {
   register,
