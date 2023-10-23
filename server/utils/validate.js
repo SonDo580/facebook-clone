f5d2cc766b1d3bdb850a5536b7cc20a8fcc2f464
Son Do <dominhson580@gmail.com>
2023-10-23 12:03:03 +0700

    Create function to validate date of birth

---
+++ server/utils/validate.js
@@ -0,0 +1,31 @@
+const MAX_AGE = 200;
+
+const validateDateOfBirth = (birthDay, birthMonth, birthYear) => {
+  if (birthDay < 1 || birthDay > 31) {
+    return "Birth Day is invalid!";
+  }
+
+  if (birthMonth < 1 || birthMonth > 12) {
+    return "Birth Month is invalid!";
+  }
+
+  const currentYear = new Date().getFullYear();
+  if (currentYear - birthYear > MAX_AGE) {
+    return "Birth Year is invalid!";
+  }
+
+  const date = new Date(birthYear, birthMonth - 1, birthDay);
+  if (
+    date.getDate() !== birthDay ||
+    date.getMonth() !== birthMonth - 1 ||
+    date.getFullYear() !== birthYear
+  ) {
+    return "Date of Birth is invalid!";
+  }
+
+  return null;
+};
+
+module.exports = {
+  validateDateOfBirth,
+};
