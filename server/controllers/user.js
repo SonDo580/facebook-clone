4f9828c164a317750d4a8c80907f984e12d02d6d
Son Do <dominhson580@gmail.com>
2023-12-19 22:37:08 +0700

    Handle unfollow user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,37 +2,65 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../models/user");
 
+/* Follow a user */
 const follow = asyncHandler(async (req, res) => {
   const currentUser = req.user;
   const currentUserId = currentUser._id.toString();
-  const userToFollowId = req.params.userId;
+  const otherUserId = req.params.userId;
 
-  if (currentUserId === userToFollowId) {
+  if (currentUserId === otherUserId) {
     res.status(403);
     throw new Error("You can't follow yourself!");
   }
 
-  if (currentUser.following.includes(userToFollowId)) {
+  if (currentUser.following.includes(otherUserId)) {
     res.status(403);
     throw new Error("You already followed this user!");
   }
 
   // Find user to follow
-  const userToFollow = await User.findById(userToFollowId);
-  if (!userToFollow) {
+  const otherUser = await User.findById(otherUserId);
+  if (!otherUser) {
     res.status(400);
     throw new Error("User not found!");
   }
 
   // Update 'following' array of current user
-  await currentUser.updateOne({ $push: { following: userToFollowId } });
+  await currentUser.updateOne({ $push: { following: otherUserId } });
 
   // Update 'followers' array of the other user
-  await userToFollow.updateOne({ $push: { followers: currentUserId } });
+  await otherUser.updateOne({ $push: { followers: currentUserId } });
 
-  res.json({ followed: userToFollowId });
+  res.json({ followed: otherUserId });
+});
+
+/* Unfollow a user */
+const unfollow = asyncHandler(async (req, res) => {
+  const currentUser = req.user;
+  const currentUserId = currentUser._id.toString();
+  const otherUserId = req.params.userId;
+
+  if (currentUserId === otherUserId) {
+    res.status(403);
+    throw new Error("You can't unfollow yourself!");
+  }
+
+  if (!currentUser.following.includes(otherUserId)) {
+    res.status(403);
+    throw new Error("You haven't followed this user!");
+  }
+
+  // Update 'following' array of current user
+  await currentUser.updateOne({ $pull: { following: otherUserId } });
+
+  // Update 'followers' array of the other user
+  const otherUser = await User.findById(otherUserId);
+  await otherUser.updateOne({ $pull: { followers: currentUserId } });
+
+  res.json({ unfollowed: otherUserId });
 });
 
 module.exports = {
   follow,
+  unfollow,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
 const protect = require("../middlewares/protect");
-const { follow } = require("../controllers/user");
+const { follow, unfollow } = require("../controllers/user");
 
 const router = Router();
 
 router.use(protect);
 
 router.put("/:userId/follow", follow);
+router.put("/:userId/unfollow", unfollow);
 
 module.exports = router;
