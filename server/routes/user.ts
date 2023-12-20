a0eae0c40d96d6c3562541d6055a40dacc29ab2b
Son Do <dominhson580@gmail.com>
2023-12-20 22:07:04 +0700

    Add routes for friend handling

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,12 +1,27 @@
 import { Router } from "express";
 
 import protect from "../middlewares/protect";
-import { follow, unfollow } from "../controllers/user";
+import {
+  acceptFriendRequest,
+  cancelFriendRequest,
+  follow,
+  friendRequest,
+  rejectFriendRequest,
+  unfollow,
+  unfriend,
+} from "../controllers/user";
 
 const router = Router();
 
 router.use(protect);
+
 router.put("/:userId/follow", follow);
 router.put("/:userId/unfollow", unfollow);
+
+router.put("/:userId/friendRequest", friendRequest);
+router.put("/:userId/cancelFriendRequest", cancelFriendRequest);
+router.put("/:userId/acceptFriendRequest", acceptFriendRequest);
+router.put("/:userId/rejectFriendRequest", rejectFriendRequest);
+router.put("/:userId/unfriend", unfriend);
 
 export default router;
