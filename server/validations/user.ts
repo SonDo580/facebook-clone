a7219afa4551d4093269a686b1211ceaae52a4f7
Son Do <dominhson580@gmail.com>
2023-12-21 23:03:57 +0700

    BE - Initial validations for updated user info

--- server/validations/user.ts
+++ server/validations/user.ts
@@ -89,6 +89,90 @@ const registerValidations: ValidationChain[] = [
     .toInt(),
 ];
 
+const updateUserValidations = [
+  body("firstName")
+    .if((_, { req }) => req.body.firstName !== undefined)
+    .trim()
+    .notEmpty()
+    .withMessage(requiredMessage("First Name"))
+    .isLength({ max: NAME_MAX_LENGTH })
+    .withMessage(lengthMessage("First Name", { max: NAME_MAX_LENGTH }))
+    .escape(),
+
+  body("lastName")
+    .if((_, { req }) => req.body.lastName !== undefined)
+    .trim()
+    .notEmpty()
+    .withMessage(requiredMessage("Last Name"))
+    .isLength({ max: NAME_MAX_LENGTH })
+    .withMessage(lengthMessage("Last Name", { max: NAME_MAX_LENGTH }))
+    .escape(),
+
+  body("email")
+    .if((_, { req }) => req.body.email !== undefined)
+    .trim()
+    .notEmpty()
+    .withMessage(requiredMessage("Email"))
+    .isEmail()
+    .withMessage(invalidMessage("Email"))
+    .normalizeEmail(),
+
+  body("password")
+    .if((_, { req }) => req.body.password !== undefined)
+    .notEmpty()
+    .withMessage(requiredMessage("Password"))
+    .isLength({ min: PASSWORD_MIN_LENGTH, max: PASSWORD_MAX_LENGTH })
+    .withMessage(
+      lengthMessage("Password", {
+        min: PASSWORD_MIN_LENGTH,
+        max: PASSWORD_MAX_LENGTH,
+      })
+    )
+    .isStrongPassword({
+      minLength: PASSWORD_MIN_LENGTH,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage(
+      "Password must contains both uppercase letters, lowecase letters, numbers, symbols!"
+    ),
+
+  body("gender")
+    .if((_, { req }) => req.body.gender !== undefined)
+    .trim()
+    .notEmpty()
+    .withMessage(requiredMessage("Gender")),
+
+  body("birthDay")
+    .if((_, { req }) => req.body.birthDay !== undefined)
+    .trim()
+    .notEmpty()
+    .withMessage(requiredMessage("Birth Day"))
+    .isInt({ min: MIN_MONTH_DAY, max: MAX_MONTH_DAY })
+    .withMessage(invalidMessage("Birth Day"))
+    .toInt(),
+
+  body("birthMonth")
+    .if((_, { req }) => req.body.birthMonth !== undefined)
+    .trim()
+    .notEmpty()
+    .withMessage(requiredMessage("Birth Month"))
+    .isInt({ min: MIN_MONTH, max: MAX_MONTH })
+    .withMessage(invalidMessage("Birth Month"))
+    .toInt(),
+
+  body("birthYear")
+    .if((_, { req }) => req.body.birthYear !== undefined)
+    .trim()
+    .notEmpty()
+    .withMessage(requiredMessage("Birth Year"))
+    .isInt()
+    .withMessage(invalidMessage("Birth Year"))
+    .toInt(),
+];
+
 const validateDateOfBirth = (
   birthDay: number,
   birthMonth: number,
@@ -119,4 +203,4 @@ const validateDateOfBirth = (
   return null;
 };
 
-export { registerValidations, validateDateOfBirth };
+export { registerValidations, updateUserValidations, validateDateOfBirth };
