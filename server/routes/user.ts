77d972ae607c7386c505f41dcaa8af5333978ea8
Son Do <dominhson580@gmail.com>
2023-12-21 21:38:13 +0700

    BE - Add routes to handle user CRUD

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 
 import protect from "../middlewares/protect";
-import { follow, unfollow } from "../controllers/user";
+import {
+  getInfo,
+  updateInfo,
+  deleteAccount,
+  follow,
+  unfollow,
+} from "../controllers/user";
 
 const router = Router();
 
 router.use(protect);
+
+router.route("/:userId").get(getInfo).put(updateInfo).delete(deleteAccount);
 
 router.put("/:userId/follow", follow);
 router.put("/:userId/unfollow", unfollow);
