7b2a7114ae25abb11aa97dcd2be38208b65361da
Son Do <dominhson580@gmail.com>
2023-12-30 11:01:18 +0700

    FE - Add route for profile page

--- client/src/config/paths.ts
+++ client/src/config/paths.ts
@@ -2,6 +2,7 @@ const PATHS = {
   home: "/",
   register: "/register",
   login: "/login",
+  profile: "/profile/:userId?",
 };
 
 export { PATHS };
--- client/src/config/routes.ts
+++ client/src/config/routes.ts
@@ -1,6 +1,7 @@
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Profile from "@/pages/Profile";
 import { PATHS } from "./paths";
 
 const PUBLIC_ROUTES = [
@@ -18,6 +19,10 @@ const PROTECTED_ROUTES = [
   {
     path: PATHS.home,
     component: Home,
+  },
+  {
+    path: PATHS.profile,
+    component: Profile,
   },
 ];
 
