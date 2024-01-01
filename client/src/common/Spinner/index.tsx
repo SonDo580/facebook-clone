b215dad48d1318dccebd300a23157ca46f5b11a1
Son Do <dominhson580@gmail.com>
2024-01-01 22:49:29 +0700

    FE - Create Spinner component

---
+++ client/src/common/Spinner/index.tsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+function Spinner() {
+  return (
+    <div className="spinnerContainer">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
