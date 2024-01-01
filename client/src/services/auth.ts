794fde80918bd13f3c056be0079d26f15a77f99c
Son Do <dominhson580@gmail.com>
2024-01-01 15:39:37 +0700

    FE - Create register service

---
+++ client/src/services/auth.ts
@@ -0,0 +1,24 @@
+import { User } from "@/types/user";
+import { RegisterData } from "@/types/auth";
+import { saveItemToLocalStorage } from "@/utils/storage";
+import { handleServiceError } from "@/utils/error";
+import { axiosInstance } from "./request";
+
+const URL = "/auth";
+
+const register = async (userData: RegisterData): Promise<User> => {
+  try {
+    const response = await axiosInstance.post<User>(
+      `${URL}/register`,
+      userData
+    );
+
+    saveItemToLocalStorage<User>("user", response.data);
+    return response.data;
+  } catch (error) {
+    return handleServiceError(error);
+  }
+};
+
+const authService = { register };
+export default authService;
---
+++ client/src/types/auth.ts
@@ -0,0 +1,12 @@
+type RegisterData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  birthDay: number;
+  birthMonth: number;
+  birthYear: number;
+  gender: string;
+};
+
+export type { RegisterData };
