795ea7761fb2cccb8211739c33b9519f0af2584a
Son Do <dominhson580@gmail.com>
2024-01-02 09:49:29 +0700

    Remove logging

--- client/src/services/request.ts
+++ client/src/services/request.ts
@@ -4,8 +4,6 @@ const baseURL = import.meta.env.PROD
   ? (import.meta.env.VITE_SERVER_URL as string)
   : "http://localhost:5000";
 
-console.log(baseURL);
-
 const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
