48e8d76c68d4d7cf6d423a70a908397313386a6e
Son Do <dominhson580@gmail.com>
2024-01-11 23:11:09 +0700

    FE - Show getPosts loading and error

--- client/src/components/Posts/index.tsx
+++ client/src/components/Posts/index.tsx
@@ -5,6 +5,7 @@ import { postSliceSelector } from "@/redux/selectors";
 
 import "./style.scss";
 import Post from "./Post";
+import Skeleton from "@/common/Skeleton";
 
 type Props = {
   posts: PostType[];
@@ -12,6 +13,14 @@ type Props = {
 
 function Posts({ posts }: Props) {
   const { loading, errorMsg } = useSelector(postSliceSelector);
+
+  if (loading) {
+    return <Skeleton count={10} />;
+  }
+
+  if (errorMsg) {
+    return <h1>{errorMsg}</h1>;
+  }
 
   return (
     <div className="posts">
