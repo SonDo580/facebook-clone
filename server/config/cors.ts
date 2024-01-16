012d94cd76fe438ee94dcb33b2b589cad179ea5a
Son Do <dominhson580@gmail.com>
2024-01-16 11:02:29 +0700

    BE - Only allow specified origins in production

--- server/config/cors.ts
+++ server/config/cors.ts
@@ -5,8 +5,21 @@ const whitelist =
     ? [process.env.CLIENT_URL!]
     : ["http://localhost:5173"];
 
+const dynamicOrigin: typeof corsOptions.origin = (origin = "", callback) => {
+  if (whitelist.includes(origin)) {
+    callback(null, true);
+  } else {
+    callback(new Error("Not allowed by CORS"));
+  }
+};
+
+// Only allow specified origins (live client) in production
+// REST tools like Postman can still be used in development
+const origin =
+  process.env.NODE_ENV === "production" ? dynamicOrigin : whitelist;
+
 const corsOptions: CorsOptions = {
-  origin: whitelist,
+  origin,
   credentials: true,
 };
 
